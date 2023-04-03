28301e1a23d6c05d6d24f00287c10d3d9eab375b
Robert Safin <robertsafin98@gmail.com>
2023-04-03 19:15:36 +0800

    card comp

---
+++ src/components/Meals/MealItem.js/MealItemForm.js
@@ -0,0 +1,16 @@
+import classes from './MealItemForm.module.css'
+
+
+
+
+
+const MealItemForm = () => {
+  return (
+    <form className={classes.form}>
+      <input type="text" />
+      <button>+ Add</button>
+    </form>
+  )
+}
+
+export default MealItemForm
